0dc33f122952bf6ece2d66b7c1e825816aab7c0b
jiseop9083 <sinphi03@gmail.com>
2025-07-13 16:17:19 +0900

    feat: init ios project

---
+++ ios/wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by jiseop9083 on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
